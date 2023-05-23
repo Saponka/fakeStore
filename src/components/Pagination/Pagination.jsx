5431d3ad56c9da51427a3e43b76271e579a2247c
Franco <francoherrera_8@hotmail.com.ar>
2023-05-22 21:18:45 -0300

    pagination from bulma + props

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({productsPerPage,totalProducts,currentPage,setCurrentPage}) => {
+
+
+
   return (
     <nav className="pagination is-centered mb-6" role="navigation" aria-label="pagination">
-  <a className="pagination-previous">Previous</a>
-  <a className="pagination-next">Next page</a>
+  <a className="pagination-previous">Anterior</a>
+  <a className="pagination-next">Siguiente</a>
   <ul className="pagination-list">
     <li><a className="pagination-link" aria-label="Goto page 1">1</a></li>
     {/* <li><span className="pagination-ellipsis">&hellip;</span></li> */}
